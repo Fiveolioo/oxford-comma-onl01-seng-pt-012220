e64045902e1299d3e4d47a181c0a9276cbd4cdae
Richard Cho <96richardohc@gmail.com>
2020-01-30 08:08:42 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,16 +1,18 @@
 def oxford_comma(array)
   puts array
-  oxford_fruits = []
   if array.length == 1
     array.join
+  elsif array.length == 2
+    array.join(" and ")
   else
-  else
+    oxford_fruits = []
     array.each do |fruit|
       if array.last == fruit
         oxford_fruits << "and #{fruit}"
       else  
-        oxford_fruits << ""
+        oxford_fruits << "#{fruit}, "
       end
     end
+    oxford_fruits.join
   end
 end
